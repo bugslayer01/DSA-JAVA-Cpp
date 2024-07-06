32b7cfae093dee037d58b1fe62b52917c7a3c53a
Devansh <devansh28112004@gmail.com>
2024-07-07 01:58:04 +0530

    done with saturday practice

--- cpp/array/duplicateCheck.cpp
+++ cpp/array/duplicateCheck.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+using namespace std;
+int main(){
+    int element=0;
+    cout<<"enter the size of array to be checked for duplicacy";
+    cin >>element;
+    int arr[element]={0};
+    cout<<"now enter the elementss one by one"<<endl;
+//getting thr array
+    for(int i=0;i<=element;i++){
+       cin>>arr[i];
+       }
+// make a new array to check for duplicacy
+   
+   
+
+    return 0;
+}
